7786dfada94003f5c4d7bd4ea7b281e3988df4cd
Seth A. Yoder <seth.a.yoder@gmail.com>
2018-12-12 21:27:32 +0000

    Add release name and maturity in version.php

--- version.php
+++ version.php
@@ -13,3 +13,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2018120400;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;        // Requires this Moodle version
 $plugin->component = 'enrol_payment';    // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_RC;
+$plugin->release   = '1.0';
